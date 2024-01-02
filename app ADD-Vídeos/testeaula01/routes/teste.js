8494906d6f5b3f232d01109c524396e0a7bd17bd
Emerson Marins Coutinho <emersonmarinscoutinho@gmail.com>
2024-01-02 20:38:32 -0300

    Delete app ADD-Vídeos directory

--- app ADD-Vídeos/testeaula01/routes/teste.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-    res.render('teste', {title:"A natureza é linda!"});
-});
-
-module.exports = router;
